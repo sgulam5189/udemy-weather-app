439df450daaf22499f58097ccc06b5c90b085905
sgulam5189 <samdanicse07@gmail.com>
2020-12-04 15:04:47 -0500

    Add files via upload
    changing port

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('../../weather-app/geocode')
 const forecast = require('../../weather-app/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -95,6 +96,6 @@ app.get('*', (req,res) =>{
 })
 
 
-app.listen(3000, () => {
-    console.log('Server Started on port 3000')
+app.listen(port, () => {
+    console.log('Server Started on port ' + port)
 })
